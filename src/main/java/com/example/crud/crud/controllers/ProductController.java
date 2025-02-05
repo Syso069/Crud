8301edd7187199c642fb33039609ecb674520c97
sisom <sisomd69@gmail.com>
2025-02-05 19:01:05 -0300

    criando funcionalidade de ler, enviar, editar e deletar

--- src/main/java/com/example/crud/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/crud/controllers/ProductController.java
@@ -1,15 +1,51 @@
 package com.example.crud.crud.controllers;
 
+import com.example.crud.crud.domain.product.Product;
+import com.example.crud.crud.domain.product.RequestProduct;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping ("/product")
 public class ProductController {
+    @Autowired
+    private ProductRepository repository;
+
     @GetMapping
-    public ResponseEntity getAllProducts(){
-        return ResponseEntity.ok("Deu ok!");
+    public ResponseEntity getAllProducts() {
+        var allProducts = repository.findAll();
+        return ResponseEntity.ok(allProducts);
+    }
+
+    @PostMapping
+    public ResponseEntity registerProduct(@RequestBody @Valid RequestProduct data){
+        Product newProduct = new Product(data);
+        repository.save(newProduct);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity<Product> updateProduct(@RequestBody @Valid RequestProduct data){
+        Optional<Product> optionalProduct = repository.findById(data.id());
+        if (optionalProduct.isPresent()){
+            Product product = optionalProduct.get();
+            product.setName(data.name());
+            product.setPrice_in_cents(data.price_in_cents());
+            return ResponseEntity.ok(product);
+        }else {
+            return ResponseEntity.ok().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable String id){
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/example/crud/crud/controllers/ProductRepository.java
+++ src/main/java/com/example/crud/crud/controllers/ProductRepository.java
@@ -1,4 +1,8 @@
 package com.example.crud.crud.controllers;
 
-public class ProductRepository {
+import com.example.crud.crud.domain.product.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, String> {
+
 }
